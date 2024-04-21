4865548704f761a96ce56925eece53149781ea24
Ankush Rajput <141709546+ankushsingh8416@users.noreply.github.com>
2024-04-21 13:48:59 +0530

    Update Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = ({ width = "100px" }) => {
   return (
     <div>
-      <img src="./public/img/logo.png" width={width} alt="" />{" "}
+      <img src="https://res.cloudinary.com/dvucqewfn/image/upload/v1713687056/logo_w89iom.png" width={width} alt="" />{" "}
     </div>
   );
 };
